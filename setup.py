1ddb9ac4afe3f4072a22afa8488805821d9d01b8
max <max@axisnode.net>
2017-05-15 02:41:47 -0700

    .

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='verifiweather',
-      version='0.1.3',
+      version='0.1.4',
       description='Verifi Job Application Weather App',
       url='http://github.com/computermax11/verifiweather.git',
       author='Max Schulberg',
