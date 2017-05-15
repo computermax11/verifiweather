7b1323625afeff5562dc9f3d11b4fd83409e4ad9
max <max@axisnode.net>
2017-05-14 20:38:44 -0700

    Verifi Weather

--- runcgi.sh
+++ runcgi.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-[[ ! -d cgi-bin ]] && mkdir cgi-bin && ln -s verifiweather/verifiweather.py cgi-bin
+[[ ! -d cgi-bin ]] && mkdir cgi-bin && ln -s verifiweather.py cgi-bin
 [[ ! -f index.html ]] && echo '<meta http-equiv="refresh" content="0; url=cgi-bin/verifiweather.py">' > index.html
 echo "The app is available at http://<your IP>:8000/cgi-bin/verifiweather.py"
 python -m CGIHTTPServer
