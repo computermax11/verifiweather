55f6e150c84a0805392977630df7745ef60a7f5a
max <max@axisnode.net>
2017-05-14 20:55:07 -0700

    Verifi Weather App

--- verifiweather/__main__.py
+++ verifiweather/__main__.py
@@ -3,10 +3,9 @@ import sys
 from verifiweather import *
 
 def main(args=None):
-    print(current_weather())
-#    if args is None:
-#        args = sys.argv[1:]
-#    print(current_weather(args))
+    if args is None:
+        args = sys.argv[1:]
+    print(current_weather(args))
 
 if __name__ == "__main__":
     main()
