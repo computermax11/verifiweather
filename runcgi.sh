f6447681b5e8dd03234285fcbf0c044b3974ad87
max <max@axisnode.net>
2017-05-15 02:57:44 -0700

    .

--- runcgi.sh
+++ runcgi.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-[[ ! -d cgi-bin ]] && mkdir cgi-bin && ln -s verifiweather.py cgi-bin
+[[ ! -f verifiweather/verifiweather.py ]] && echo "run from source directory" && exit 1
+BPATH=$(pwd)
+[[ ! -d cgi-bin ]] && mkdir cgi-bin 
+[[ ! -h cgi-bin/verifiweather.py ]] && ln -s ${BPATH}/verifiweather/verifiweather.py ${BPATH}/cgi-bin
 [[ ! -f index.html ]] && echo '<meta http-equiv="refresh" content="0; url=cgi-bin/verifiweather.py">' > index.html
 echo "The app is available at http://<your IP>:8000/cgi-bin/verifiweather.py"
 python -m CGIHTTPServer
